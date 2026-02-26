efa7f1b1de037cf07cccb983e22fa1ccb1698213
PrithakBhattarai <bhattaraiprithak12345@gmail.com>
2026-02-26 16:48:59 -0600

    Create ActionInitialization.hh

---
+++ ActionInitialization.hh
@@ -0,0 +1,13 @@
+#ifndef ACTIONINITIALIZATION_HH
+#define ACTIONINITIALIZATION_HH
+
+#include "G4VUserActionInitialization.hh"
+
+class ActionInitialization : public G4VUserActionInitialization {
+public:
+    ActionInitialization() {}
+    virtual ~ActionInitialization() {}
+    virtual void Build() const override;
+};
+
+#endif
